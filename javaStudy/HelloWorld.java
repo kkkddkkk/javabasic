a6abc51279eddead59312cb3e2571b2b5ee7b6fe
kkkddkkk <fullstack.kindokyeong@gmail.com>
2025-05-12 17:44:24 +0900

    init commit

---
+++ javaStudy/HelloWorld.java
@@ -0,0 +1,13 @@
+package javaStudy;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕. 난 김도경이야");
+		
+		//문자셋 설정: 이 설정이 각각 틀리면, 한글 데이터나 주석이 깨질 수 있음. 반드시 초기에 해둘 것! 모든 문자셋은 UTF-8로 설정
+		//
+	}
+
+}
