cdf7e03f726635c1ade1fb4ce54b73e9b3b7ed58
seol <fullstack.eunseolgo14@gmail.com>
2025-05-12 17:59:31 +0900

    [고은설] 인사 메세지 추가

--- javaStudy/DKClass.java
+++ javaStudy/DKClass.java
@@ -5,6 +5,8 @@ public class DKClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("도경입니당~");
+		System.out.println("안녕하세요 도경님, 잘 왔다 갑니다!! ");
+
 	}
 
 }
